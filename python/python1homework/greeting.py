a12915729b0a10bb832a392eb9768abf7555ae75
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-03-27 21:50:45 +0100

    Lab 2, Objective 1

---
+++ python/python1homework/greeting.py
@@ -0,0 +1,5 @@
+"""Lab2 Objective 1"""
+firstname = input("Please enter your first name: ")
+lastname = input("Please enter your last name: ")
+print("I am pleased to meet you", firstname, lastname)
+
---
+++ python/python1homework/lab1quiz2.py
@@ -0,0 +1,2 @@
+print ("This is a very fun class\
+and I am learning about Python!")
