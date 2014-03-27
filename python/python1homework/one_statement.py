82e1f37911688ceaa7c82f037b29ee86d3a00bb5
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-03-27 21:38:15 +0100

    Lesson 1, objective 1

---
+++ python/python1homework/one_statement.py
@@ -0,0 +1,14 @@
+#Lesson 1, objective 1
+# This simple project is designed to remind you how to create and run a program, and to show how to submit homework projects.. Go through the following steps, referring to the course notes if you get stuck.
+# Create a new file.
+# Enter the four lines of code shown in the editor window below:
+# "Simple one-statement module with docstring."
+# print("""This is a very simple program
+# that, when run, will verify I can create,
+# save, and run programs in CodeRunner.""")
+# Save the file (we recommend creating and using a folder for this and all of your homework separate from your /python1 folder with a recognizable name like /python1homework) as one_statement.py.
+# Run it to make sure it works properly.
+"Simple one-statement module with docstring."
+print("""This is a very simple program
+that, when run, will verify I can create,
+save, and run programs in CodeRunner.""")
