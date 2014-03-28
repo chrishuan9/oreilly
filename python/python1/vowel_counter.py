f7b8446a410d46dd76d4194a6d5887b3de17659a
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-03-28 17:16:00 +0100

    Lesson 4: Iteration: For Loops and Iteration

---
+++ python/python1/vowel_counter.py
@@ -0,0 +1,9 @@
+__author__ = 'chris'
+#Python 1: Lesson 4: Iteration: For and While Loops
+"""Counts the vowels in a user input string."""
+s = input("Enter any string: ")
+vcount = 0
+for c in s:
+    if c in "aeiouAEIOU":
+        vcount += 1
+    print("C is ", c, "; Vowel count:", vcount);
