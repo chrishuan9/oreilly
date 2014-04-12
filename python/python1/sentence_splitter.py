94c951261ac948d950b5b7e33b6dfd5ef7315342
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-12 21:24:22 +0200

    Lesson 4: For and While Loops: Sentence splitter - doesn't work with multiple spaces

---
+++ python/python1/sentence_splitter.py
@@ -0,0 +1,14 @@
+__author__ = 'chris'
+"""Program to split a sentence into words."""
+s = input("Please enter a sentence: ")
+while True:
+    pos = 0
+    for c in s:
+        if c == " ":
+            print(s[:pos])
+            s = s[pos + 1:]
+            break
+        pos += 1
+    else:
+        print(s)
+        break
