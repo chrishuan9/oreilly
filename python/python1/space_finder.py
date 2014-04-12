60cd986bbbc6331611c15a9ce5996c71c60bd5bd
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-12 10:23:16 +0200

    Lesson 4: For and While Loops: modified space finder to accommodate case where no space is found in String

--- python/python1/space_finder.py
+++ python/python1/space_finder.py
@@ -5,6 +5,13 @@ s = input("Enter any string: ")
 pos = 0
 for c in s:
     if c == " ":
+        print("First space occurred at position ", pos)
         break
     pos += 1
-print("First space occurred at position", pos)
+else:
+    #Python loops come with such extra logic built in, in the shape of the optional else clause. This clause is placed
+    # at the same indentation level as the for or while loop that it matches, and (just as with the if statement) is
+    # followed by an indented suite of one or more statements. This suite is only executed if the loop terminates
+    # normally.
+    # If the loop ends because a break statement is executed, then the interpreter just skips over the else suite.
+    print("No spaces in that string ")
