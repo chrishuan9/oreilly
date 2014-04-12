6df44dcb69742bfbe202fe791aab2a46c055f434
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-12 21:38:56 +0200

    Lesson 4: For and While Loops: sentence splitter - checks case where multiple spaces are inserted between terms

--- python/python1/sentence_splitter.py
+++ python/python1/sentence_splitter.py
@@ -2,6 +2,11 @@ __author__ = 'chris'
 """Program to split a sentence into words."""
 s = input("Please enter a sentence: ")
 while True:
+    # removes double spacing between words
+    # as long as there is a space between the words use string slicing to remove the space from the string
+    # by slicing the string from the second character thereby omitting the space
+    while s.startswith(" "):
+        s = s[1:]
     pos = 0
     for c in s:
         if c == " ":
