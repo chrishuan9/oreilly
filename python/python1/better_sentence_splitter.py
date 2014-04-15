822adc7248963689e77c0557a9d960247b7315ed
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-15 23:50:39 +0200

    Lesson 5: Sequence Containers

---
+++ python/python1/better_sentence_splitter.py
@@ -0,0 +1,8 @@
+__author__ = 'chris'
+#!/usr/local/bin/python3
+"""Simpler program to list the words of a string."""
+
+s = input("Enter your string: ")
+words = s.strip().split()
+for word in words:
+    print(word)
