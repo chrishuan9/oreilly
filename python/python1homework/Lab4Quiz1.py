4294e636575f506bcd01ce1b67094bf3fbbd7f3d
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-18 04:33:33 +0200

    Lesson 4: For and While Loops: Lab4, Quiz 1 completed

--- python/python1homework/Lab4Quiz1.py
+++ python/python1homework/Lab4Quiz1.py
@@ -1,8 +1,13 @@
 __author__ = 'chris'
+"""
+Write a for statement to bind each character of the lower-case alphabet to the name "c"
+(there is no need to write a loop body).
+"""
 #String in python are immutable, therefore a string cannot be treated as a list
 inputstring = "testlala"
-for c in inputstring:
-    if c in 'abcdefghijklmnopqrstuvwxyz':
-        inputstring
-        c = 'c'
+for i in range(len(inputstring)):
+    if inputstring[i] in 'abcdefghijklmnopqrstuvwxyz':
+        #string are immutable - modifying a string actually creates a new object
+        #we need to bind the new object to the variable
+        inputstring = inputstring.replace(inputstring[i], 'c')
 print(inputstring)
