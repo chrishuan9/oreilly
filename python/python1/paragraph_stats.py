73c465737e5296d4f4e7a98d99f8bdae44f90154
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-04-18 23:56:08 +0200

    Python 1: Lesson 5: Sequence Containers

---
+++ python/python1/paragraph_stats.py
@@ -0,0 +1,22 @@
+__author__ = 'chris'
+#!/usr/local/bin/python3
+"""Count the words, lines and characters in a chunk of text."""
+
+gettysburg = """\Four score and seven years ago ourfathers brought forth on this continent,a new nation,
+conceived in Liberty, anddedicated to the proposition thatall men are created equal.
+Now we are engaged in a great civil war,testing whether that nation, orany nation so conceived and so dedicated,
+can long endure. We are met ona great battle-field of that war. We havecome to dedicate a portion of thatfield,
+as a final resting place for thosewho here gave their lives that thatnation might live. It is altogetherfitting
+and proper that we should do this."""
+
+charct = len(gettysburg)
+
+lines = gettysburg.split("\n")
+linect = len(lines)
+
+wordct = 0
+for line in lines:
+    words = line.split()
+    wordct += len(words)
+
+print("The text contains", linect, "lines,", wordct, "words, and", charct, "characters.")
