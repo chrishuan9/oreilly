a95ab9582f20a5c76fe28f20b0118e5845fa164b
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-09 10:31:59 +0200

    Lab 5, Objective 1: counting Uppercase and lowercase words

---
+++ python/python1homework/word_list.py
@@ -0,0 +1,30 @@
+__author__ = 'chris'
+"""This project tests your ability to use sequences.
+
+Create a new Python source file named word_list.py.
+Make the program read a string from the user and create a list of words from the input.
+Create two lists, one containing the words that contain at least one upper-case letter and one of the words that don't contain any upper-case letters.
+Use a single for loop to print out the words with upper-case letters in them, followed by the words with no upper-case letters in them, one word per line.
+Verify that your program works correctly, and hand it in.
+"""
+
+def hasUpperCase(word):
+    for character in word:
+        if character.isupper():
+            return True
+
+
+s = input("Enter your string: ")
+words = s.strip().split()
+upperCaseList = []
+noUpperCaseList = []
+for word in words:
+    if (hasUpperCase(word)):
+        print("Adding '" + word + "' to uppercase list")
+        upperCaseList+=word
+    else:
+        print("Adding '" + word + "' to nouppercase list")
+        noUpperCaseList+=word
+print(upperCaseList)
+print(noUpperCaseList)
+
