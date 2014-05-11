c30482fe74d1654be56972fbde34ae2dc49c81c2
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-11 19:57:46 +0200

    replaced if - statement with single line

--- python/python1/word_frequency.py
+++ python/python1/word_frequency.py
@@ -16,10 +16,7 @@ Who lives down the lane."""
 for punc in ",?;.":
     text = text.replace(punc, "")
 freq = {}
-for word in text.lower().split()
-    if word in freq:
-        freq[word] +=1
-    else:
-        freq[word] = 1
+for word in text.lower().split():
+    freq[word] = freq.get(word,0)+1
 for word in sorted(freq.keys()):
     print(word, freq[word])
