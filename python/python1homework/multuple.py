bd9ed4a955ea168b4d4f785bc904f559f83f7af9
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-18 23:41:54 +0200

    added skeleton for Lab 7, Objective 1

---
+++ python/python1homework/multuple.py
@@ -0,0 +1,21 @@
+__author__ = 'chris'
+# Lab7, Objective 1
+"""
+Objective 1:
+This project tests your ability to handle string formatting.
+
+Create a new Python source file named multuple.py.
+Write a program that takes as data a tuple of two-element tuples, such as ((1, 1), (2, 2), (12, 13), (4, 4), (99, 98)).
+This and/or similar data should be hard-coded (no need for user input).
+Loop over the tuple and print out the results of multiplying the two numbers together, and use string formatting to
+display nicely.
+Below is an example of output from running the program with the sample data above. Preserve the spacing, assuming that no input number is greater than 99.
+
+   1 =  1 x  1
+   4 =  2 x  2
+ 156 = 12 x 13
+  16 =  4 x  4
+9702 = 99 x 98
+"""
+data = ((1, 1), (2, 2), (12, 13), (4, 4), (99, 98))
+print(data)
