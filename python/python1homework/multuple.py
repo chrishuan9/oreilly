f9977cfdca993a74a163d36de75a7096fbb8d182
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-19 00:31:58 +0200

    finished Lab7, Quiz 1

--- python/python1homework/multuple.py
+++ python/python1homework/multuple.py
@@ -18,4 +18,13 @@ Below is an example of output from running the program with the sample data abov
 9702 = 99 x 98
 """
 data = ((1, 1), (2, 2), (12, 13), (4, 4), (99, 98))
-print(data)
+
+# Formatting syntax:
+#
+# >: The field is right-aligned in the available space, with any padding to its left.
+# 4,2,2: Minimum field width, Note that unless a minimum field width is defined, the field width will always be the
+# same size as the data to fill it, so that the alignment option has no meaning in this case.
+# d: Denotes the type, determines how the data should be presented = Decimal: formats the integer in base 10.
+#
+for tup in data:
+    print("{0:>4d} = {1:2d} x {2:2d}".format(tup[0]*tup[1],tup[0],tup[1]))
