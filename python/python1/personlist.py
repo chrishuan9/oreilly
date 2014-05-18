88f2a36675679bb6b1ae2be4b56afd129402ac90
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-18 15:19:28 +0200

    added thorough explanation about the used formatting syntax

--- python/python1/personlist.py
+++ python/python1/personlist.py
@@ -6,5 +6,17 @@ data = [
     ("Simon", 39, 155),
     ("David", 61, 135)
 ]
+# format syntax explanation: for details see <https://docs.python.org/3.3/library/string.html>
+##
+# 0 : arg_name: either number or a keyword: number = positional argument, keyword = named keyword,
+# it denotes the specific parameter passed by in the format(arg_name_1, arg_name_2, etc).
+# [0]: element_index: denotes the specific element in tuple: 0: String, 1: first integer, 2: second integer
+# :< : '<' Forces the field to be left-aligned within the available space (this is the default for most objects)
+# 12 : Minimum field width, Note that unless a minimum field width is defined, the field width will always be the same
+# size as the data to fill it, so that the alignment option has no meaning in this case.
+# s/d: Denotes the type, determines how the data should be presented
+# 's': String format. This is the default type for strings and may be omitted.
+# None: Same as 's'
+# 'd': Decimal Integer. Outputs the number in base 10.
 for row in data:
     print("{0[0]:<12s} {0[1]:4d} {0[2]:4d}".format(row))
