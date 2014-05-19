377a7413e4b6e5c6be0420b04518f834d7b02d61
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-19 10:09:43 +0200

    eliminated inner loop

--- python/python1/factorial.py
+++ python/python1/factorial.py
@@ -8,3 +8,14 @@ while (f < 1000):
     f = 1
     for n in range(c,0,-1):
         f = f * n
+#
+# if you already know N!, then you can produce (N+1)! (the next value in the sequence) by multiplying N! by N+1.
+# You can make this program even more efficient by avoiding the second loop, since the second loop would be run
+# for each factorial. This saves a lot of work.
+#
+c = 1
+f = 1
+while (f < 1000):
+    print(f)
+    c += 1
+    f *= c
