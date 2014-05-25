429957380ef90357a525ff801f713959940dd55d
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-25 21:54:07 +0200

    Python Lesson 11: Lab 1: Quiz 1

---
+++ python/python1homework/lab11quiz1.py
@@ -0,0 +1,8 @@
+__author__ = 'chris'
+"""
+Question 1:
+Write a function that returns the string "This is now lowercase:" plus the lowercase representation of its string argument, as a single string.
+"""
+
+def to_lower_case(text):
+    return "This is now lowercase: " + text.lower()
