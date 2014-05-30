01753185f19c32c790468b3954d26bd0816498de
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 21:47:58 +0200

    Python 1: Lesson 14: Classes and object-oriented programming: refactoring - removed old constructor

--- python/python1/cplx.py
+++ python/python1/cplx.py
@@ -1,14 +1,9 @@
 __author__ = 'chris'
 """Initial implementation of complex numbers."""
 
+
 class Cplx:
     pass
-
-def cplx(real, imag):
-    c = Cplx()
-    c.real = real
-    c.imag = imag
-    return c
 
 
 def cinit(c, real, imag):
@@ -22,8 +17,10 @@ def cadd(c1, c2):
     c.imag = c1.imag + c2.imag
     return c
 
+
 def cstr(c):
     return "%s+%sj" % (c.real, c.imag)
+
 
 if __name__ == "__main__":
     zero = Cplx()
