0ac373ec9899c81aa085ece90fbcf5c694902201
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 20:48:03 +0200

    Python 1: Lesson 13: More about functions - Lab 13, Quiz 2: Examples

---
+++ python/python1homework/lab13quiz2.py
@@ -0,0 +1,9 @@
+__author__ = 'chris'
+
+
+def posArgumentToKeyParameteR(number1=1, number2=2, text="Text"):
+    print("Number 1: {0} , Number 2: {1}, Text: {2}".format(number1, number2, text))
+
+
+if __name__ == "__main__":
+    posArgumentToKeyParameteR(number3=3, numbesr4=4, text="Hello");
