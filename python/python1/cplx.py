a0ed41e92c2bf5117f72ebb6ab3df6e58612a9c9
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 22:15:04 +0200

    Python 1: Lesson 14: Classes and object-oriented programming: Python Magic: __init__ hook

--- python/python1/cplx.py
+++ python/python1/cplx.py
@@ -6,14 +6,12 @@ class Cplx:
     #pass statement no longer needed because class body is not empty anymore
     #pass
 
-    def cinit(c, real, imag):
+    def __init__(c, real, imag):
         c.real = real
         c.imag = imag
 
     def cadd(c1, c2):
-        c = Cplx()
-        c.real = c1.real + c2.real
-        c.imag = c1.imag + c2.imag
+        c = Cplx(c1.real + c2.real, c1.imag + c2.imag)
         return c
 
     def cstr(c):
@@ -21,13 +19,8 @@ class Cplx:
 
 
 if __name__ == "__main__":
-    zero = Cplx()
-    #Methods need to be prefixed with the class name
-    #because methods were moved into the class
-    zero.cinit(zero, 0.0, 0.0)
-    one = Cplx()
-    one.cinit(one, 1.0, 0.0)
-    i = Cplx()
-    i.cinit(i, 0.0, 1.0)
-    result = zero.cadd(zero, one.cadd(one, i))
-    print(result.cstr(result))
+    zero = Cplx(0.0, 0.0)
+    one = Cplx(1.0, 0.0)
+    i = Cplx(0.0, 1.0)
+    result = zero.cadd(one.cadd(i))
+    print(result.cstr())
