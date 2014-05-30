56891dc3d18929cd655cbcb90344b23312e22602
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 22:00:49 +0200

    Python 1: Lesson 14: Classes and object-oriented programming: movied methods into class

--- python/python1/cplx.py
+++ python/python1/cplx.py
@@ -5,21 +5,18 @@ __author__ = 'chris'
 class Cplx:
     pass
 
+    def cinit(c, real, imag):
+        c.real = real
+        c.imag = imag
 
-def cinit(c, real, imag):
-    c.real = real
-    c.imag = imag
+    def cadd(c1, c2):
+        c = Cplx()
+        c.real = c1.real + c2.real
+        c.imag = c1.imag + c2.imag
+        return c
 
-
-def cadd(c1, c2):
-    c = Cplx()
-    c.real = c1.real + c2.real
-    c.imag = c1.imag + c2.imag
-    return c
-
-
-def cstr(c):
-    return "%s+%sj" % (c.real, c.imag)
+    def cstr(c):
+        return "%s+%sj" % (c.real, c.imag)
 
 
 if __name__ == "__main__":
