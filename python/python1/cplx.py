b4378c808bb1464845fef04d983664b24757fdee
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 22:18:30 +0200

    Python 1: Lesson 14: Classes and object-oriented programming: parameter overloading is a breeze

--- python/python1/cplx.py
+++ python/python1/cplx.py
@@ -10,7 +10,7 @@ class Cplx:
         c.real = real
         c.imag = imag
 
-    def cadd(c1, c2):
+    def __add__(c1, c2):
         c = Cplx(c1.real + c2.real, c1.imag + c2.imag)
         return c
 
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     zero = Cplx(0.0, 0.0)
     one = Cplx(1.0, 0.0)
     i = Cplx(0.0, 1.0)
-    result = zero.cadd(one.cadd(i))
+    result = zero + (one + i)
     print(result)
