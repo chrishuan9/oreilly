8a617adc6405d60902c1d8aec35453685aa8ddb9
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-31 01:08:59 +0200

    Python 1: Lesson 14: Lab 14, Objective 1 - not working yet

--- python/python1homework/doggies.py
+++ python/python1homework/doggies.py
@@ -3,14 +3,15 @@ __author__ = 'chris'
 Objective 1:
 This project tests more of your understanding of classes and objects.
 
-Create a new Python source file named doggies.py.
-Write a class named Dog. Dog's __init__() method should take two parameters, name and breed, in addition to the
+1. Create a new Python source file named doggies.py.
+2. Write a class named Dog. Dog's __init__() method should take two parameters, name and breed, in addition to the
 implicit self.
-Bind an empty list to a dogs global variable (dogs should not be an attribute of the Dog class).
-Using a while loop, get user input for name and breed. The loop should terminate when the user enters a blank name.
-For each name and breed entered, create an instance of the Dog class with name and breed as arguments. Append the
+3. Bind an empty list to a dogs global variable (dogs should not be an attribute of the Dog class).
+4. Using a while loop, get user input for name and breed. The loop should terminate when the user enters a blank name.
+5. For each name and breed entered, create an instance of the Dog class with name and breed as arguments. Append the
 object to the dogs list.
-Each time around the loop, print the current dogs list (the name and breed of each dog).
+6. Each time around the loop, print the current dogs list (the name and breed of each dog).
+
 Below is an example of possible output from running the program.
 
 Name: Snoopy
@@ -33,3 +34,24 @@ DOGS
 ****************************************
 Name:
 """
+
+
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def __str__(self):
+        print("{0}:{1}".format(name, breed))
+
+
+if __name__ == "__main__":
+    dogs = []
+    while True:
+        inputName = input("Name:")
+        inputBreed = input("Breed:")
+        dogs.append(Dog(inputName, inputBreed))
+        for i, dog in enumerate(dogs):
+            print("*" * 41)
+            print("{0}. {1}".format(i, dog))
+
