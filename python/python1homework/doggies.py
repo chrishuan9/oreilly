e54b6b51113c621d81d7c6c12311aae02dd223f8
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 22:22:52 +0200

    Python 1: Lesson 14: Lab 14, Objective 1: Skeleton

---
+++ python/python1homework/doggies.py
@@ -0,0 +1,35 @@
+__author__ = 'chris'
+"""
+Objective 1:
+This project tests more of your understanding of classes and objects.
+
+Create a new Python source file named doggies.py.
+Write a class named Dog. Dog's __init__() method should take two parameters, name and breed, in addition to the
+implicit self.
+Bind an empty list to a dogs global variable (dogs should not be an attribute of the Dog class).
+Using a while loop, get user input for name and breed. The loop should terminate when the user enters a blank name.
+For each name and breed entered, create an instance of the Dog class with name and breed as arguments. Append the
+object to the dogs list.
+Each time around the loop, print the current dogs list (the name and breed of each dog).
+Below is an example of possible output from running the program.
+
+Name: Snoopy
+Breed: Beagle
+DOGS
+0. Snoopy:Beagle
+****************************************
+Name: Marmaduke
+Breed: Great Dane
+DOGS
+0. Snoopy:Beagle
+1. Marmaduke:Great Dane
+****************************************
+Name: Rover
+Breed: Mutt
+DOGS
+0. Snoopy:Beagle
+1. Marmaduke:Great Dane
+2. Rover:Mutt
+****************************************
+Name:
+"""
