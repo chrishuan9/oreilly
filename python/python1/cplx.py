eff32db6207862b91855ded83a38c203a141a4bd
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-30 21:41:25 +0200

    Python 1: Lesson 14: Classes and object-oriented programming: refactoring

--- python/python1/cplx.py
+++ python/python1/cplx.py
@@ -1,10 +1,8 @@
 __author__ = 'chris'
 """Initial implementation of complex numbers."""
 
-
 class Cplx:
     pass
-
 
 def cplx(real, imag):
     c = Cplx()
@@ -13,20 +11,26 @@ def cplx(real, imag):
     return c
 
 
+def cinit(c, real, imag):
+    c.real = real
+    c.imag = imag
+
+
 def cadd(c1, c2):
     c = Cplx()
     c.real = c1.real + c2.real
     c.imag = c1.imag + c2.imag
     return c
 
-
 def cstr(c):
     return "%s+%sj" % (c.real, c.imag)
 
-
 if __name__ == "__main__":
-    zero = cplx(0.0, 0.0)
-    one = cplx(1.0, 0.0)
-    i = cplx(0.0, 1.0)
+    zero = Cplx()
+    cinit(zero, 0.0, 0.0)
+    one = Cplx()
+    cinit(one, 1.0, 0.0)
+    i = Cplx()
+    cinit(i, 0.0, 1.0)
     result = cadd(zero, cadd(one, i))
     print(cstr(result))
