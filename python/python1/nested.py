4980b9bf5fceb744ed1674e56beb413b5ab85acd
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-31 13:13:35 +0200

    Python 1: Lesson 15: Exception Handling: Nested exceptions

---
+++ python/python1/nested.py
@@ -0,0 +1,41 @@
+__author__ = 'chris'
+""" Nested exception handling
+
+Example output:
+====================
+a:  1 / b:  string
+Invalid types for division
+None
+====================
+a:  string / b:  0
+Invalid types for division
+None
+====================
+a:  2 / b:  0
+Divide by zero
+None
+====================
+a:  123 / b:  4
+30.75
+
+"""
+
+
+def divide(a, b):
+    """ Return result of dividing a by b """
+    print("=" * 20)
+    print("a: ", a, "/ b: ", b)
+    try:
+        try:
+            return a / b
+        except TypeError:
+            print("Invalid types for division")
+    except ZeroDivisionError:
+        print("Divide by zero")
+
+
+if __name__ == "__main__":
+    print(divide(1, "string"))
+    print(divide("string", 0))
+    print(divide(2, 0))
+    print(divide(123, 4))
