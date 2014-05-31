d593dc4d15d36efe2d330a68aa02fc19dbef1723
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-05-31 23:39:21 +0200

    Python 1: Lesson 16: Refactoring Example - added new functionality

--- python/python1/refactor.py
+++ python/python1/refactor.py
@@ -2,16 +2,20 @@ __author__ = 'chris'
 """Demonstrates an opportunity for refactoring."""
 
 
-def list_multiply(a, b):
+def list_multiply(*lists):
     """ Sums two lists of integers and multiplies them together
 
     >>> list_multiply([3,4],[3,4])
     49
     >>> list_multiply([1,2,3,4],[10,20])
     300
+    >>> list_multiply([4,3,2,1],[50,50],[5,5,5])
+    15000
     """
-
-    return sum(a) * sum(b)
+    total = 1
+    for l in lists:
+        total *= sum(l)
+    return total
 
 
 def _test():
