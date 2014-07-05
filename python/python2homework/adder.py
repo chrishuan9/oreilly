4f7394b3301e604a2c661abb18a430255faa9dc1
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-05 22:27:30 +0200

    Python 2: Lesson 3: Test Driven Development: Test Example - still failes: fixed failing test

--- python/python2homework/adder.py
+++ python/python2homework/adder.py
@@ -4,9 +4,9 @@ __author__ = 'chris'
 import numbers
 
 def adder(x, y):
-    if isinstance(x, list):
+    if isinstance(x, list) and not isinstance(y, list):
         return x + [y]
-    elif isinstance(y, list):
+    elif isinstance(y, list) and not isinstance(x, list):
         return y + [x]
     elif isinstance(x, numbers.Number) and isinstance(y, str):
         return str(x) + y
