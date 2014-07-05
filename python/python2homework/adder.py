19573d6aeb61d5e732a4fa8384d9b79b49abee1f
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-05 22:26:21 +0200

    Python 2: Lesson 3: Test Driven Development: Test Example - still failes: not so stupid implementation

--- python/python2homework/adder.py
+++ python/python2homework/adder.py
@@ -1,7 +1,13 @@
 __author__ = 'chris'
 "adder.py: defines an adder function according to a slightly unusual " \
 "definition."
-
+import numbers
 
 def adder(x, y):
+    if isinstance(x, list):
+        return x + [y]
+    elif isinstance(y, list):
+        return y + [x]
+    elif isinstance(x, numbers.Number) and isinstance(y, str):
+        return str(x) + y
     return x + y
