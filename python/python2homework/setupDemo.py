e9e6766e1471f8d1143b86e65519732326435682
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-07 23:23:08 +0200

    Python 2: Lesson 3: Test Driven Development: Homework: Project 1 - fixed test1()

---
+++ python/python2homework/setupDemo.py
@@ -0,0 +1,52 @@
+__author__ = 'chris'
+"""
+
+The test_1() method includes code to verify that the test directory contains
+only the files created by the for loop. Hint: You might create a set
+containing the list of three filenames, and then create a set from the
+os.listdir() method.
+
+A test_3() method creates a binary file that contains exactly a million bytes,
+closes it and then uses os.stat to verify that the file on disk is of
+the correct length (with os.stat, statinfo.st_size returns the size in bytes).
+
+"""
+import unittest
+import tempfile
+import shutil
+import glob
+import os
+
+
+class FileTest(unittest.TestCase):
+    def setUp(self):
+        self.origdir = os.getcwd()
+        self.dirname = tempfile.mkdtemp("testdir")
+        # print("Created", self.dirname)
+        os.chdir(self.dirname)
+
+    def test_1(self):
+        "Verify creation of files is possible"
+        filenamelist = ["this.txt", "that.txt", "the_other.txt"]
+        for filename in filenamelist:
+            f = open(filename, "w")
+            f.write("Some text\n")
+            f.close()
+            self.assertTrue(f.closed)
+        files = os.listdir(self.dirname);
+        # need to sort the lists because python won't treat the lists as
+        #being equal if the order of the element differs
+        self.assertEqual(filenamelist.sort(), files.sort())
+
+    def test_2(self):
+        "Verify that the current directory is empty"
+        self.assertEqual(glob.glob("*"), [], "Directory not empty")
+
+    def tearDown(self):
+        os.chdir(self.origdir)
+        shutil.rmtree(self.dirname)
+        # print("Deleted", self.dirname)
+
+
+if __name__ == "__main__":
+    unittest.main()
