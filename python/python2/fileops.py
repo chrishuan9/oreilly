fe5660409c91f9b08cfd3d44a1e242a1a52ae2fc
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-10 19:23:02 +0200

    Python 2: Lesson 4: File Handling: implemented stub methods

--- python/python2/fileops.py
+++ python/python2/fileops.py
@@ -6,11 +6,18 @@ def write_list(fn, lst):
     """Writes a list to a named file. Each list item will be on
     a separate line. Overwrites the file if it already exists.
     """
-    pass
+    # 'w' for only writing (an existing file with the same name will be erased)
+    f = open(fn, "w")
+    for item in lst:
+        f.write("%s\n" % item)
+    f.close()
 
 
 def read_list(fn):
     """Reads a list from a file without using readline.
     Uses standard line endings ("\n") to delimit list items.
     """
-    pass
+    f = open(fn, "r")
+    s = f.read()
+    l = s.split("\n")
+    return l
