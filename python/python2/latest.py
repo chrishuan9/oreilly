e5be09b24e1782d588229d6d143db0770f990cf7
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-13 02:27:38 +0200

    Python 2: Lesson 4: File Handling: test passes

--- python/python2/latest.py
+++ python/python2/latest.py
@@ -3,4 +3,13 @@ import glob
 import os
 
 def latest(num=1, path="."):
-    pass
+    files_with_dates = []
+    files = glob.glob(os.path.join(path, "*"))
+    latest_files = []
+    for fn in files:
+        files_with_dates.append((os.path.getmtime(fn), os.path.abspath(fn)))
+    files_with_dates.sort()
+    for file_info in files_with_dates[-num:]:
+        latest_files.append(file_info[1])
+    latest_files.reverse()
+    return latest_files
