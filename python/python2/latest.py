fd96acfe31b8918d11955081a0a93cff86fbf4ad
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-14 18:33:09 +0200

    Python 2: Lesson 4: File Handling: refactored using decorators and list comprehension

--- python/python2/latest.py
+++ python/python2/latest.py
@@ -5,11 +5,8 @@ import os
 def latest(num=1, path="."):
     files_with_dates = []
     files = glob.glob(os.path.join(path, "*"))
-    latest_files = []
-    for fn in files:
-        files_with_dates.append((os.path.getmtime(fn), os.path.abspath(fn)))
-    files_with_dates.sort()
-    for file_info in files_with_dates[-num:]:
-        latest_files.append(file_info[1])
+    dated_files = [(os.path.getmtime(fn), os.path.abspath(fn)) for fn in files]
+    dated_files.sort()
+    latest_files = [f for (d, f) in dated_files[-num:]]
     latest_files.reverse()
     return latest_files
