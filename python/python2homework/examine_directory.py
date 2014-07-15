e1134579cfe06e4573e47b3dd345b1115c854e61
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-07-15 04:09:41 +0200

    Python 2: Lesson 4: File Handling: Project 1: Stub Implementation / preparing tests

---
+++ python/python2homework/examine_directory.py
@@ -0,0 +1,12 @@
+__author__ = 'chris'
+import glob
+import os
+
+
+def latest(path="."):
+    files = glob.glob(os.path.join(path, "*"))
+    dated_files = [(os.path.getmtime(fn), os.path.abspath(fn)) for fn in files]
+    dated_files.sort()
+    latest_files = [f for (d, f) in dated_files[-num:]]
+    latest_files.reverse()
+    return latest_files
---
+++ python/python2homework/test_examine_directory.py
@@ -0,0 +1,49 @@
+__author__ = 'chris'
+import unittest
+import tempfile
+import latest
+import time
+import os
+
+# Make a FileHandling_Homework project and assign it to your Python2_Homework
+# working set. In that project, write a module containing a function to
+# examine the contents of the current working directory and print out a count
+# of how many files have each extension (".txt", ".doc", etc.)
+
+# PATHSTEM = "v:\\workspace\\FileHandling\\src\\"
+PATHSTEM = "/Users/chris/Documents/dev/oreilly/python/python2homework/"
+
+
+class TestLatest(unittest.TestCase):
+    def setUp(self):
+        self.path = PATHSTEM
+        self.tempdirectory = tempfile.mkdtemp("testdir")
+        self.dummy_files = ["file1.doc", "file1.txt", "file2.txt"]
+        for fn in self.dummy_files:
+            f = open(self.tempdirectory + fn, "w")
+            f.close()
+            time.sleep(1)
+
+    def test_empty_directory(self):
+        """
+        Ensure that calling the function on an empty directory returns
+        nothing / empty list
+        """
+
+    def test_current_directory(self):
+        """
+        Ensure that calling the function with no arguments returns
+        the contents of the current directory
+        """
+        expected = [self.path + "file.new"]
+        latest_file = latest.latest(path=self.path)
+        self.assertEqual(latest_file, expected, )
+
+
+    def tearDown(self):
+        for fn in self.dummy_files:
+            os.remove(self.tempdirectory + fn)
+
+
+if __name__ == "__main__":
+    unittest.main()
