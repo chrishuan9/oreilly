84fb43f0fd3ee6f6e9ac29088cce0d3f272270ec
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-09-01 14:17:44 +0200

    cleaned up the code

--- python/python2homework/test_examine_directory.py
+++ python/python2homework/test_examine_directory.py
@@ -11,17 +11,19 @@ import os
 # of how many files have each extension (".txt", ".doc", etc.)
 
 # PATHSTEM = "v:\\workspace\\FileHandling\\src\\"
-PATHSTEM = "/Users/chris/Documents/dev/oreilly/python/python2homework/"
+# PATHSTEM = "/Users/chris/Documents/dev/oreilly/python/python2homework/"
 
 
 class TestLatest(unittest.TestCase):
     def setUp(self):
-        self.path = PATHSTEM
+        self.cwd = os.getcwd()
         self.tempdirectory = tempfile.mkdtemp("testdir")
+        print("Created Temp Directory ", self.tempdirectory)
         self.dummy_files = ["file1.doc", "file1.txt", "file2.txt"]
+        os.chdir(self.tempdirectory)
         for fn in self.dummy_files:
-            print(os.path.join(self.tempdirectory, fn))
-            f = open(os.path.join(self.tempdirectory,fn), "w")
+            print(fn)
+            f = open(fn, "w")
             f.close()
             time.sleep(1)
 
@@ -30,20 +32,17 @@ class TestLatest(unittest.TestCase):
         Ensure that calling the function on an empty directory returns
         nothing / empty list
         """
-        # self.emptydirectory = tempfile.TemporaryDirectory("emptydir")
         self.emptydirectory = tempfile.mkdtemp("emptydir")
         expected = {}
         extension_count = examine_directory.examine(path=self.emptydirectory)
         self.assertEqual(extension_count, expected)
-        # self.emptydirectory.cleanup()
-        #os.remove(self.emptydirectory)
-        #self.emptydirectory.
-        #self.assertFalse(os.path.exists(self.emptydirectory))
+        #cleanup
+        shutil.rmtree(self.emptydirectory)
 
     def test_current_directory(self):
         """
         Ensure that calling the function with no arguments returns
-        the contents of the current directory
+        the contents of the current working directory
         """
         expected = {".doc": 1, ".txt": 2}
         extensions_count = examine_directory.examine(path=self.tempdirectory)
@@ -51,8 +50,9 @@ class TestLatest(unittest.TestCase):
 
 
     def tearDown(self):
-        for fn in self.dummy_files:
-            os.remove(os.path.join(self.tempdirectory,fn))
+        os.chdir(self.cwd)
+        shutil.rmtree(self.tempdirectory)
+        print("Deleted ", self.tempdirectory)
 
 
 if __name__ == "__main__":
