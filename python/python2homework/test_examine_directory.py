20c97b0d3046fb09ddf54f2a5778c60923dcc8da
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-09-01 14:44:57 +0200

    Python 2: Lesson 4: File Handling: Project 1: bugfix

--- python/python2homework/test_examine_directory.py
+++ python/python2homework/test_examine_directory.py
@@ -4,15 +4,12 @@ import tempfile
 import examine_directory
 import time
 import os
+import shutil
 
 # Make a FileHandling_Homework project and assign it to your Python2_Homework
 # working set. In that project, write a module containing a function to
 # examine the contents of the current working directory and print out a count
 # of how many files have each extension (".txt", ".doc", etc.)
-
-# PATHSTEM = "v:\\workspace\\FileHandling\\src\\"
-# PATHSTEM = "/Users/chris/Documents/dev/oreilly/python/python2homework/"
-
 
 class TestLatest(unittest.TestCase):
     def setUp(self):
@@ -38,6 +35,7 @@ class TestLatest(unittest.TestCase):
         self.assertEqual(extension_count, expected)
         #cleanup
         shutil.rmtree(self.emptydirectory)
+        print("Deleted ", self.emptydirectory)
 
     def test_current_directory(self):
         """
