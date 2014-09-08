8adaa30532caa5fd36e6d901ec1c7807bf357786
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-09-08 13:27:45 +0200

    stubs for project1

---
+++ python/python2homework/L5_Project1/test_highscore_table.py
@@ -0,0 +1,17 @@
+import unittest
+import library
+import os
+import glob
+
+
+class TestLibrary(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+
+ if __name__ == "__main__":    unittest.main()
