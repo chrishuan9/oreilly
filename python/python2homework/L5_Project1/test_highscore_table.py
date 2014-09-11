78b70f657a39b3c4bb17a1421cb1add92bc1774a
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-09-11 23:47:41 +0200

    implemented additional tests

--- python/python2homework/L5_Project1/test_highscore_table.py
+++ python/python2homework/L5_Project1/test_highscore_table.py
@@ -6,26 +6,30 @@ import highscoretable
 
 class TestLibrary(unittest.TestCase):
     def setUp(self):
-        pass
+        self.lib_fn = r'/Users/chris/Documents/dev/oreilly/python/python2homework/L5_Project1/highscore.shelve'
 
     def tearDown(self):
-        pass
+        shelve_files = glob.glob(self.lib_fn + '*')
+        for fn in shelve_files:
+            os.remove(fn)
 
 
     def testNewPlayerNewHighScore(self):
         #add new score
         highscore = highscoretable.writehighscore('chris',2)
         self.assertEqual(highscore, 2)
-        new_highscore = highscoretable.writehighscore('chris',5)
-        self.assertEqual(new_highscore, 5)
-        same_highscore =  highscoretable.writehighscore('chris',4)
-        self.assertEqual(same_highscore, 5)
 
-
-    def testExistingPlayerNewHighScore(self):
-        pass
 
     def testExistingPlayerOverwritingHighScore(self):
-        pass
+        highscore = highscoretable.writehighscore('chris',4)
+        self.assertEqual(highscore, 4)
+        same_highscore =  highscoretable.writehighscore('chris',5)
+        self.assertEqual(same_highscore, 5)
+
+    def testExistingPlayerExistingHighScore(self):
+        highscore = highscoretable.writehighscore('chris',5)
+        self.assertEqual(highscore, 5)
+        same_highscore = highscoretable.writehighscore('chris',4)
+        self.assertEqual(same_highscore, 5)
 
     if __name__ == "__main__":    unittest.main()
