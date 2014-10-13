2bd294473bbe547bba7498c040144b98fc2791f6
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-10-13 19:00:11 +0200

    Event propagation

--- python/python2/L8_MoreGUI/evtreport.py
+++ python/python2/L8_MoreGUI/evtreport.py
@@ -6,8 +6,15 @@ root = Tk()
 def handler(event):
     print("Keystroke '{0}' ({1}) {2} ".format(event.char, len(event.char), event.keycode))
 
+
+def handler2(event):
+    print("RootKeystroke '{0}' ({1}) {2} ".format(event.char, len(event.char), event.keycode))
+
 frame = Frame(root, width=100, height=100)
-frame.bind("<Key>", handler)
+#frame.bind("<Key>", handler)
+frame.bind("o", handler)
+frame.bind("k", handler)
+root.bind("<Key>", handler2)
 frame.pack()
 frame.focus()
 root.mainloop()
