f2bce01526414c81f8f88d561beda6a488ba605f
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-10-13 09:50:20 +0200

    Lesson 8 homework assignment: building a simple ui

---
+++ python/python2homework/L8_Project1/buildingUIs.py
@@ -0,0 +1,26 @@
+__author__ = 'chris'
+"""
+Write a GUI-based program to build a window layout as shown below. When the frame is resized,
+the buttons should stay the same height and expand sideways. Frame 1 and Frame 2 should always be
+the same height and width as each other, and Frame 3 should be half as wide again as they are
+(i.e. 150% wider, as shown below).  Labeling each Frame is optional / good exercise.
+
+ +---------------------+--------------------------------+
+ |                     |                                |
+ |                     |                                |
+ |                     |                                |
+ |      Frame 1        |                                |
+ |                     |                                |
+ |                     |                                |
+ |                     |                                |
+ +---------------------+               Frame 3          |
+ |                     |                                |
+ |                     |                                |
+ |                     |                                |
+ |     Frame 2         |                                |
+ |                     |                                |
+ |                     |                                |
+ +----------+----------+----------+----------+----------+
+ | Button 1 | Button 2 | Button 3 | Button 4 | Button 5 |
+
+"""
