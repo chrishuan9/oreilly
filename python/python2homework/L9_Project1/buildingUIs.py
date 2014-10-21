ba64ae96717c73b6e1907ce21a8442ce91fc4892
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-10-21 09:58:47 +0200

    Python 2: Lesson 9: More GUI: Fixed button alignment at the bottom by adding them to their respective frames

--- python/python2homework/L9_Project1/buildingUIs.py
+++ python/python2homework/L9_Project1/buildingUIs.py
@@ -52,7 +52,7 @@ class Application(Frame):
         self.master.columnconfigure(2, weight=3)
         self.grid(sticky=ALL)
 
-        # Frame1
+        # #################################################################### Frame1
         self.f1 = Frame(master, bg="red", name="frame_1")
         self.f1.grid(row=0, column=0, rowspan=1, columnspan=2, sticky=ALL)
 
@@ -61,52 +61,64 @@ class Application(Frame):
         self.label1.pack(fill=BOTH, expand=True)
 
 
-        #Frame2
+        ##################################################################### Frame2
         self.f2 = Frame(master, bg="blue", name="frame_2")
         self.f2.grid(row=1, column=0, rowspan=1, columnspan=2, sticky=ALL)
 
         #using pack manager inside the frame for the label
         self.label2 = Label(self.f2, text="Frame 2", bg="blue")
-        self.label2.pack(fill=BOTH, expand=True)
+        self.label2.grid(row=0, column=0, columnspan=2, sticky=ALL)
+        self.f2.rowconfigure(0, weight=1)
+        self.f2.rowconfigure(1, weight=0)
+        self.f2.columnconfigure(0, weight=1)
+        self.f2.columnconfigure(1, weight=1)
 
+        bred = Button(self.f2, text="Red", command=self.bredhandler)
+        bblue = Button(self.f2, text="Blue", command=self.bbluehandler)
 
-        #Frame 3
-        self.f3 = Frame(master, bg="green", name="frame _3")
+        bred.grid(row=1, column=0, sticky=W + E)
+        bblue.grid(row=1, column=1, sticky=W + E)
+
+        # #################################################################### Frame3
+        # uses grid geometry manager
+        self.f3 = Frame(master, name="frame _3")
         self.f3.grid(row=0, column=2, rowspan=3, columnspan=3, sticky=ALL)
 
-        # using pack manager inside the frame for the entry and text widget
+        # ensures that the textdisplay field is maximized while the entry
+        # and button row are kept at their minimum size
+        self.f3.rowconfigure(1, weight=1)
+        self.f3.columnconfigure(0, weight=1)
+        self.f3.columnconfigure(1, weight=1)
+        self.f3.columnconfigure(2, weight=1)
+
+        # entry and text field
         self.entryfield = Entry(self.f3)
-        self.textdisplay = Text(self.f3, state=DISABLED, wrap=WORD, )
-        self.entryfield.pack(side=TOP, fill=X)
-        self.textdisplay.pack(side=BOTTOM, fill=BOTH, expand=True)
+        self.entryfield.grid(row=0, column=0, columnspan=3, sticky=W + E)
+        self.textdisplay = Text(self.f3, state=DISABLED, wrap=WORD)
+        self.textdisplay.grid(row=1, column=0, columnspan=3, sticky=ALL)
+
+        # adding buttons on the last row of Frame 3
+        self.bgreen = Button(self.f3, text="Green", command=self.bgreenhandler)
+        self.bblack = Button(self.f3, text="Black", command=self.bblackhandler)
+        self.bopen = Button(self.f3, text="Open", command=self.bopenhandler)
+
+        self.bgreen.grid(row=2, column=0, sticky=W + E)
+        self.bblack.grid(row=2, column=1, sticky=W + E)
+        self.bopen.grid(row=2, column=2, sticky=W +E)
+
 
         # Writing text requires the widget to be in an active state
         self.textdisplay.configure(state=NORMAL)
         self.textdisplay.insert(INSERT, "NO FILE LOADED")
         self.textdisplay.configure(state=DISABLED)
 
-        #Frame 4 for the buttons
-        self.f4 = Frame(master, bg="white", name="frame_4")
-        self.f4.grid(row=2, column=0, rowspan=1, columnspan=5, sticky=ALL)
 
-        #Buttons
-        bred = Button(self.f4, text="Red", command=self.bredhandler)
-        bblue = Button(self.f4, text="Blue", command=self.bbluehandler)
-        bgreen = Button(self.f4, text="Green", command=self.bgreenhandler)
-        bblack = Button(self.f4, text="Black", command=self.bblackhandler)
-        bopen = Button(self.f4, text="Open", command=self.bopenhandler)
-
-        bred.pack(side=LEFT, fill=X, expand=TRUE)
-        bblue.pack(side=LEFT, fill=X, expand=TRUE)
-        bgreen.pack(side=LEFT, fill=X, expand=TRUE)
-        bblack.pack(side=LEFT, fill=X, expand=TRUE)
-        bopen.pack(side=LEFT, fill=X, expand=TRUE)
 
         self.label1.bind("<Button-1>", self.mousehandler)
         self.label2.bind("<Button-1>", self.mousehandler)
 
 
-    # ButtonHandler
+    ##################################################################### ButtonHandlers
     def bredhandler(self):
         self.textdisplay.configure(fg="red")
         return "break"
@@ -148,7 +160,7 @@ class Application(Frame):
         return "break"
 
 
-    # event handler for mouse click
+    ##################################################################### event handler for mouse clicks in Frame 1/2
     def mousehandler(self, event):
         print("{0} was clicked at x:{1} and y:{2}".format(
             str(event.widget)[1:8], event.x, event.y))
