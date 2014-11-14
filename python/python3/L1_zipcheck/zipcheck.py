6cbbccde0c963a39be7837fdf888b707676ed9b6
Chris Yereaztian <chris.yereaztian@gmail.com>
2014-11-14 15:36:26 +0100

    fixed implementation

--- python/python3/L1_zipcheck/zipcheck.py
+++ python/python3/L1_zipcheck/zipcheck.py
@@ -8,7 +8,15 @@ def zip_errors(z):
     """
     Validate z as either NNNNN or NNNNN-NNNN.
     """
-    pass
+    l = len(z)
+    if l not in (5,10):
+        return "Zip codes should be 5 or 10 characters long"
+    if (not z[:5].isdigit() or
+        len(z) == 10 and not z[6:].isdigit()):
+        return "Zip code contains non-numeric characters"
+    if l == 10 and z[5] != "-":
+        return "Ten-digit zips must have a dash between the two parts"
+    return
 
 if __name__ == '__main__':
     pass
